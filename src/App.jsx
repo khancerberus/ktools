cd510b6a0f89f07f125a59ad738f049457f2d5a4
Victor Valenzuela <khancerberus@hotmail.cl>
2023-10-06 23:25:47 -0300

    edit for test

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,8 @@ const App = () => {
 
   return (
     <div className="container-fluid text-center">
-      <h1 className="">KTOOLS First heading</h1>
-      <h2 className="">KTOOLS Second heading</h2>
-      <h3 className="">KTOOLS Third heading</h3>
-      <h4 className="">KTOOLS Fourth heading</h4>
-      <h5 className="">KTOOLS Fifth heading</h5>
-      <h6 className="">KTOOLS Sixth heading</h6>
-      <p className="">Hola este es un parrafo, aca va la descripcion del proyecto</p>
+      <h1 className="">TEST CODEKHAN.DEV</h1>
+      <p className="">LANDING PAGE</p>
     </div>
   );
 }
